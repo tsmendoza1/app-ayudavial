88169ed12ba3fc0aea500524b43aa8b6db5c0d0f
cazhangallimbay <cazhangallimbay@sudamericano.edu.ec>
2021-11-30 15:59:24 -0500

    Algunos correcciones
    comentada la parte de roles en
    app routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,8 +13,8 @@ import { PerfilComponent } from './Perfil/perfil/perfil.component';
 import { AngularFireAuthGuard, canActivate } from '@angular/fire/compat/auth-guard';
 import { map } from 'rxjs/operators';
 
-const uidAdmin = "OUcJ2XWr0cdwftOB7XeY"
-const onlyAdmin = () => map((user:any) =>  !!user && uidAdmin === user.uid);
+//const uidAdmin = "OUcJ2XWr0cdwftOB7XeY"
+//const onlyAdmin = () => map((user:any) =>  !!user && uidAdmin === user.uid);
 
 
 const routes: Routes = [
@@ -22,9 +22,9 @@ const routes: Routes = [
   {path: '',component:InicioComponent},
   {path: 'miauto',component:MiautoComponent},
   {path: 'mensajes',component:MensajesComponent},
-  {path: 'miperfil',component:PerfilComponent, canActivate:[AngularFireAuthGuard]}, 
+  {path: 'miperfil',component:PerfilComponent,},  //canActivate:[AngularFireAuthGuard] 
   {path: 'menu',component:MenuuComponent},
-  {path: 'ajustes',component:AjustesComponent, ... canActivate (onlyAdmin)},
+  {path: 'ajustes',component:AjustesComponent, }, //... canActivate (onlyAdmin)
   {path: 'login',component:LoginComponent},  
   {path: 'ubicacion',component:UbicacionComponent},  
   {path: 'pagos',component:PagosComponent}
