8e94ed9aabc94bc0c35c6d3b45fa62073aa977a1
cazhangallimbay <cazhangallimbay@sudamericano.edu.ec>
2021-12-23 10:06:17 -0500

    Revert "ubi"
    This reverts commit 4ef243093a2da920f6d85c7e7111259b9023ed18, reversing
    changes made to b8fadf249b25b4eaeac68a632b3b7be56eb66fc6.

---
+++ src/app/pasaste/pasaste.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pasaste',
+  templateUrl: './pasaste.component.html',
+  styleUrls: ['./pasaste.component.scss'],
+})
+export class PasasteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
